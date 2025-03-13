89c0fab3ab0ff239ad3ecb0891bbb194af5d0028
GARRY <GARRY@LAPTOP-16N9S854>
2025-03-13 11:34:11 +0700

    Update ProjectUTS_Entity.java

--- src/projectuts_entity/ProjectUTS_Entity.java
+++ src/projectuts_entity/ProjectUTS_Entity.java
@@ -15,7 +15,7 @@ public class ProjectUTS_Entity {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("");
+        System.out.println("hhhh");
     }
     
 }
