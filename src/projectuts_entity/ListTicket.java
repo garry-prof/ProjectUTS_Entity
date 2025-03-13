28719e18300a1f99b1c693f69a64fdd9f47a514c
GARRY <garrygarcia2005@gmail.com>
2025-03-13 23:03:55 +0700

    pembaruan

--- src/projectuts_entity/ListTicket.java
+++ src/projectuts_entity/ListTicket.java
@@ -8,12 +8,12 @@ package projectuts_entity;
  *
  * @author GARRY
  */
-public class LIstTicket extends javax.swing.JFrame {
+public class ListTicket extends javax.swing.JFrame {
 
     /**
      * Creates new form LIstTicket
      */
-    public LIstTicket() {
+    public ListTicket() {
         initComponents();
     }
 
@@ -59,20 +59,21 @@ public class LIstTicket extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LIstTicket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ListTicket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LIstTicket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ListTicket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LIstTicket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ListTicket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LIstTicket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ListTicket.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new LIstTicket().setVisible(true);
+                new ListTicket().setVisible(true);
             }
         });
     }
